a32f4f4130e5010a1ed4dfc8865614deafcb8f1f
edurech901 <eduardo.rech901@gmail.com>
2026-05-18 20:08:02 -0300

    spring initialize

---
+++ src/main/java/dlq/servico_auditor_dlq/ServicoAuditorDlqApplication.java
@@ -0,0 +1,13 @@
+package dlq.servico_auditor_dlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoAuditorDlqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoAuditorDlqApplication.class, args);
+	}
+
+}
